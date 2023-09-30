0d59ea7c9282d1df476483d52a4d2191db568710
diego <diego@DESKTOP-GCO3L7G>
2023-09-30 17:08:59 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*(Sueñas alto es el poder
+Que te han dado desde el cielo)
+Qué no se a dónde voy
+No es real
+Hace ya tiempo te volviste uno más
+Y odio cuanto estoy
+Lleno de este veneno y oigo truenos si no estás
+¿Qué me has hecho? ¿Dónde estoy?
+Se me aparecen mil planetas de repente
+Esto es una alucinación
+Quiero ver tu otra mitad
+Alejarme de esta ciudad
+Y contagiarme de tu forma de pensar
+Miro al cielo al recordar
+Me doy cuenta una vez más
+Que no hay momento que pase sin dejarte de pensar
+Esta distancia no es normal
+Ya me he cansado de esperar
+Dos billetes para amarte no quiero ver nada más
+Imposible, es demasiado tarde
+Todo es un desastre
+Esto es una obsesión
+No me sirven tus pocas señales
+Ya nada es como antes
+Me olvido de quien soy
+¿Qué me has hecho?
+¿Dónde estoy?
+No vas de frente, es lo de siempre
+Y de repente estoy perdiendo la razón
+Cien complejos sin sentido
+Me arrebatan tus latidos y tu voz
+Y ya no puedo más
+Qué no se a donde voy
+No es real
+Hace ya tiempo te volviste uno más
+Y odio cuanto estoy
+Lleno de este veneno y oigo truenos si no estás
+Imposible, es demasiado tarde
+Todo es un desastre
+Esto es una obsesión
+No me sirven tus pocas señales
+Ya nada es como antes
+Me olvido de quien soy
+¿Y dónde estás?
+La verdad es que ya van mil noches malditas sin tu abrazo
+Es algo raro estoy viciado a tu amor
+Quiero verte, verte, verte
+Que se acabe ya*/
